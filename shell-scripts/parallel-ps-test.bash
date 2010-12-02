d861b5a709f713ab437949de510390f3ae546db8
Sam Zaydel <szaydel@gmail.com>
2010-12-02 06:46:39 -0800

    Added Kinman\'s Recommendations to make script more flexible.

--- shell-scripts/parallel-ps-test.bash
+++ shell-scripts/parallel-ps-test.bash
@@ -6,6 +6,7 @@ WORK_DIR=/var/tmp
 CMD_TO_EXEC=/usr/bin/ps
 CMD_OPTS=-ef
 CMD_WITH_OPTS="${CMD_TO_EXEC} ${CMD_OPTS}"
+CMD_S=$(basename "${CMD_TO_EXEC}")
 CONTROL_FILE="${WORK_DIR}/ps_test_start.flag"
 
 
@@ -20,16 +21,28 @@ CONTROL_FILE="${WORK_DIR}/ps_test_start.flag"
                 while [[ "${COUNT}" -gt 0 ]]
                     do
                         ## (1) Create Log files to capture 'ps' and error output
-                        OUTPUT_LOG="${WORK_DIR}/ps-test-${COUNT}.log"
-                        ERR_LOG="${WORK_DIR}/ps-test-${COUNT}-err.log"
+                        OUTPUT_LOG="${WORK_DIR}/${CMD_S}-${COUNT}.log"
+                        ERR_LOG="${WORK_DIR}/${CMD_S}-${COUNT}-err.log"
                         
                         ## (2) Actually execute process in a loop structure
                         [[ "${DEBUG}" -ge "1" ]] && echo "Counter is currently: ${COUNT}"
                         ## If debug is enabled 'ps' will not run
                         printf "%s\n" "STDOUT Log: ${OUTPUT_LOG}"
                         [[ "${DEBUG}" -ge "1" ]] || ${CMD_WITH_OPTS} > "${OUTPUT_LOG}" 2>"${ERR_LOG}" &
-                        COUNT=$((${COUNT} - 1))
+                        
+                        ## (3) Establish return code from above, and if not '0'
+                        ## then we stop, and remove our control file, without which script stops
+                        RET_CODE = $?
+                        if [[ "${RET_CODE}" -ne "0" ]]
+                            then
+                                COUNT="0"
+                                rm "${CONTROL_FILE}"
+                            else 
+                                COUNT=$((${COUNT} - 1))
+                        fi
+                    
                     done
                 sleep "${DELAY_P}"
             done
     fi
+exit "${RET_CODE:-0}"
