618c29ed66565bc698924809628d49e0e881c7b2
Sam Zaydel <szaydel@gmail.com>
2010-12-20 12:26:28 -0800

    Initial commit. Framework for a script used to find and replace parts of files based on matching regex and writing out corrected contents to another file.

---
+++ python_scripts/replace_contents_of_file.py
@@ -0,0 +1,15 @@
+#!/bin/python3.1
+## We read contents of a file, make necessary
+## adjustements using regex
+## and write out changed contents to new file
+f_in = open('/tmp/file.in','r')
+f_out = open('/tmp/file.out','a')
+p = re.compile('s*.[0-9]{1,3}')
+read_f_in = f_in.readlines()
+for line in read_f_in:
+    if p.search(line):
+        line = re.sub(p,'relacement',line)
+        print('Wrote out to file f_out',line)
+        f_out.writelines(line)
+    else:
+        f_out.writelines(line)
