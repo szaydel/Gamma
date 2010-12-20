7bb371292cb35d367e98edd72431e56d5c157868
Sam Zaydel <szaydel@gmail.com>
2010-12-20 12:47:24 -0800

    Enhancements made to script.

--- python_scripts/check_uid_and_exec_shell_cmd.py
+++ python_scripts/check_uid_and_exec_shell_cmd.py
@@ -19,6 +19,8 @@ file_list = sys.argv[1:]
 ## with open("/tmp/stdout.txt","wb") as out:
 
 ## We first determine if the user is root or non-root
+conf_files = ['/tmp/test1.file','/tmp/test2.file','/tmp/test3.file','/tmp/test4.file']
+/tmp/a-test10.file','/tmp/a-test1.file','/tmp/a-test2.file','/tmp/a-test3.file','/tmp/a-test4.file'
 def verify_user():
     myname = pwd.getpwnam(getpass.getuser())
     ## or this is another way
@@ -39,9 +41,28 @@ x = (ret_code)
 
 if x is not 0:
     print('Do something here...')
-else:
-    print('Do something-else here...')
     
+    
+    
+    
+else:
+
+conf_files = ['/tmp/test1.file','/tmp/test2.file','/tmp/test3.file','/tmp/test4.file']
+for x in conf_files:
+    pre = '.DR.test'
+    prod_file = x
+    dr_file = prod_file+pre
+    print(dr_file,prod_file)
+    z = os.path.exists(prod_file)
+    if z is True:
+        ## If the prod version is newer than dr version alert!
+        dstamp_dr = os.stat(dr_file).st_mtime
+        dstamp_prod = os.stat(prod_file).st_mtime
+        print('File', dr_file, 'exists, and is dated:', datetime.datetime.fromtimestamp(float(dstamp_dr)))
+        if dstamp_prod >= dstamp_dr:
+            print('File: ', dr_file, 'is older than', prod_file)
+        else:
+            print('Good')
 
 ### Open the file for writing first
 #with open("/tmp/stderr.txt","wb") as PIPE:
