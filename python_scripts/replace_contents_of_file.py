5ac9cdf8bca893e722ea580cb2b9a3d727ebaaac
Sam Zaydel <szaydel@gmail.com>
2011-01-03 10:59:42 -0800

    Work-in-Progress commit after creating a few new .py snippets.

--- python_scripts/replace_contents_of_file.py
+++ python_scripts/replace_contents_of_file.py
@@ -5,6 +5,7 @@
 f_in = open('/tmp/file.in','r')
 f_out = open('/tmp/file.out','a')
 p = re.compile('s*.[0-9]{1,3}')
+## Create variable to 
 read_f_in = f_in.readlines()
 for line in read_f_in:
     if p.search(line):
